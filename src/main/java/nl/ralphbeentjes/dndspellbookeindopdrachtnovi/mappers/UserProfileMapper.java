52bc53194486782d2f0786fb3ca0d65a356b7514
Ralph-Beentjes <ralphsimonbeentjes@gmail.com>
2026-04-01 12:59:29 +0200

    Mappers toegevoegd

--- src/main/java/nl/ralphbeentjes/dndspellbookeindopdrachtnovi/mappers/UserProfileMapper.java
+++ src/main/java/nl/ralphbeentjes/dndspellbookeindopdrachtnovi/mappers/UserProfileMapper.java
@@ -1,4 +1,24 @@
 package nl.ralphbeentjes.dndspellbookeindopdrachtnovi.mappers;
 
-public class UserProfileMapper {
+import nl.ralphbeentjes.dndspellbookeindopdrachtnovi.dtos.userprofiles.UserProfileResponseDTO;
+import nl.ralphbeentjes.dndspellbookeindopdrachtnovi.entities.SpellbookEntity;
+import nl.ralphbeentjes.dndspellbookeindopdrachtnovi.entities.UserProfileEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+import java.util.Set;
+
+@Mapper(uses = {SpellbookMapper.class})
+public interface UserProfileMapper {
+
+    UserProfileMapper INSTANCE = Mappers.getMapper(UserProfileMapper.class);
+
+    @Mapping(target = "spellbooks", source = "spellbooks")
+    UserProfileEntity toEntity(Set<SpellbookEntity> spellbooks);
+
+    UserProfileResponseDTO toResponseDTO(UserProfileEntity userProfile);
+
+    List<UserProfileResponseDTO> toResponseDTOList(List<UserProfileEntity> userProfiles);
 }
