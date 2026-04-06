78099037cb5a1c8bd200e7d2b3c83b2e007ff07e
Ralph-Beentjes <ralphsimonbeentjes@gmail.com>
2026-04-06 15:46:04 +0200

    UserProfile aangepast

--- src/main/java/nl/ralphbeentjes/dndspellbookeindopdrachtnovi/dtos/userprofiles/UserProfileRequestDTO.java
+++ src/main/java/nl/ralphbeentjes/dndspellbookeindopdrachtnovi/dtos/userprofiles/UserProfileRequestDTO.java
@@ -5,16 +5,7 @@ import nl.ralphbeentjes.dndspellbookeindopdrachtnovi.entities.SpellbookEntity;
 import java.util.Set;
 
 public class UserProfileRequestDTO {
-    private String username;
     private Set<SpellbookEntity> spellbooks;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 
     public Set<SpellbookEntity> getSpellbooks() {
         return spellbooks;
--- src/main/java/nl/ralphbeentjes/dndspellbookeindopdrachtnovi/dtos/userprofiles/UserProfileResponseDTO.java
+++ src/main/java/nl/ralphbeentjes/dndspellbookeindopdrachtnovi/dtos/userprofiles/UserProfileResponseDTO.java
@@ -6,7 +6,6 @@ import java.util.Set;
 
 public class UserProfileResponseDTO {
     private Long id;
-    private String username;
     private Set<SpellbookResponseDTO> spellbooks;
 
     public Long getId() {
@@ -15,14 +14,6 @@ public class UserProfileResponseDTO {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
     }
 
     public Set<SpellbookResponseDTO> getSpellbooks() {
