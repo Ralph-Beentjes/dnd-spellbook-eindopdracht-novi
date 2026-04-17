52ae1664f03f324c348a660dc9b2284d092451cb
Ralph-Beentjes <ralphsimonbeentjes@gmail.com>
2026-04-17 16:53:56 +0200

    Kleine updates doorgevoerd

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -36,8 +36,8 @@ VALUES ('Wizard', 'A scholarly magic-user who casts spells through study and int
 
 INSERT INTO spells (spell_name, level, casting_time, range, components, duration, concentration, description)
 VALUES
-    ('Fire Bolt', 0, 'ACTION', 120, 'V, S', 'Instantaneous', false, 'You hurl a mote of fire...'),
-    ('Mage Hand', 0, 'ACTION', 30, 'V, S', '1 minute', false, 'A spectral hand appears...'),
+    ('Fire Bolt', 0, 'ACTION', 120, 'V, S', 'Instantaneous', false, 'You hurl a mote of fire at a creature or object within range. Make a ranged spell attack against the target. On a hit, the target takes 1d10 fire damage. A flammable object hit by this spell ignites if it isn’t being worn or carried.'),
+    ('Mage Hand', 0, 'ACTION', 30, 'V, S', '1 minute', false, 'A spectral, floating hand appears at a point you choose within range. The hand lasts for the duration or until you dismiss it as an action. The hand vanishes if it is ever more than 30 feet away from you or if you cast this spell again. You can use your action to control the hand. You can use the hand to manipulate an object, open an unlocked door or container, stow or retrieve an item from an open container, or pour the contents out of a vial. You can move the hand up to 30 feet each time you use it. The hand can’t attack, activate magical items, or carry more than 10 pounds.'),
     ('Prestidigitation', 0, 'ACTION', 10, 'V, S', 'Up to 1 hour', false, 'Minor magical effects...'),
     ('Sacred Flame', 0, 'ACTION', 60, 'V, S', 'Instantaneous', false, 'Radiant flame descends...'),
     ('Guidance', 0, 'ACTION', 0, 'V, S', 'Concentration, up to 1 minute', true, 'Add 1d4 to ability check...'),
